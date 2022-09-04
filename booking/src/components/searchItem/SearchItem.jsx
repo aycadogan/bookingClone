78233ed0f5923894e76bf369b901583c8e371e2c
aycadogan89 <doganayca89@gmail.com>
2022-09-03 23:21:47 -0700

    my first commit

---
+++ booking/src/components/searchItem/SearchItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './searchItem.css'
+const SearchItem = () => {
+  return (
+    <div className='searchItem'>
+      <img
+        src='https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1'
+        alt=''
+        className='siImg'
+      />
+      <div className='siDesc'></div>
+      <div className='siDetails'></div>
+    </div>
+  )
+}
+
+export default SearchItem
